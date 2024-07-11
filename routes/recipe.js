5cdf2d90be89a509b58c1e750309733c93b28134
amirabayoumi <149677044+amirabayoumi@users.noreply.github.com>
2024-07-11 20:55:39 +0200

    controller and routes

--- routes/recipe.js
+++ routes/recipe.js
@@ -4,6 +4,20 @@ import recipeControllers from '../controllers/recipe.js';
 
 const router = express.Router();
 
-// routes
+
+// GET all 
+router.get('/recipes', verifyToken, recipeControllers.getAllRecipes);
+
+// GET one 
+router.get('/recipes/:id', verifyToken, recipeControllers.getOneRecipe);
+
+// create 
+router.post('/recipes', verifyToken, recipeControllers.postRecipe);
+
+// update 
+router.put('/recipes/:id', verifyToken, recipeControllers.updateRecipe);
+
+// DELETE 
+router.delete('/recipes/:id', verifyToken, recipeControllers.deleteRecipe);
 
 export default router;
